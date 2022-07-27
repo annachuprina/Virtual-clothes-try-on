3b966302e19f76c200e944bf6b4b75ddafc9315e
anna <anna>
2022-07-27 22:08:27 +0200

    Upated in webgl files

--- src/App.js
+++ src/App.js
@@ -45,6 +45,7 @@ function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
   const [scene, changeScene] = useState(1);
+  const [keypointsVis, setVisability] = useState(true);
 
 
   const runPosenet = async () => {
@@ -76,8 +77,15 @@ function App() {
       // Make Detections
       const pose = await net.estimateSinglePose(video);
       //console.log(JSON.stringify(pose));
-      if (scene === 1)
-        unityContext1.send("Keypoints", "GetValues", JSON.stringify(pose))
+      if (scene === 1){
+        for (let i = 5; i < 11; i++ ){
+          if (pose.keypoints[i].position.x == 0 || pose.keypoints[i].position.x == 1)
+            setVisability(false)
+        }
+        if(keypointsVis == true){
+          unityContext1.send("Keypoints", "GetValues", JSON.stringify(pose))
+        }
+      }
       else if (scene === 2)
         unityContext2.send("Keypoints", "GetValues", JSON.stringify(pose))
       else if (scene === 3)
@@ -126,11 +134,17 @@ function App() {
             height: "480px",
           }}
         /> 
-        {scene === 1 && <Unity unityContext={unityContext1} className="Unity"/> }
-        {scene === 2 && <Unity unityContext={unityContext2} className="Unity"/> }
-        {scene === 3 && <Unity unityContext={unityContext3} className="Unity"/> }
-        {scene === 4 && <Unity unityContext={unityContext4} className="Unity"/> }
-        {scene === 5 && <Unity unityContext={unityContext5} className="Unity"/> }
+        {scene == 1 && keypointsVis == true && <Unity unityContext={unityContext1} className="Unity"/> }
+        {scene == 2 && keypointsVis == true && <Unity unityContext={unityContext2} className="Unity"/> }
+        {scene == 3 && keypointsVis == true && <Unity unityContext={unityContext3} className="Unity"/> }
+        {scene == 4 && keypointsVis == true && <Unity unityContext={unityContext4} className="Unity"/> }
+        {scene == 5 && keypointsVis == true && <Unity unityContext={unityContext5} className="Unity"/> }
+
+        {scene == 1 && keypointsVis == false && <div className="message"/> }
+        {scene == 2 && keypointsVis == false && <div className="message"/> }
+        {scene == 3 && keypointsVis == false && <div className="message"/> }
+        {scene == 4 && keypointsVis == false && <div className="message"/> }
+        {scene == 5 && keypointsVis == false && <div className="message"/> }
       </header>
 
       <div class="container">
@@ -162,20 +176,20 @@ function App() {
 export default App;
 
 // {"score":0.0030962747302563753,"keypoints":
-// [{"score":0.0005129482597112656,"part":"nose","position":{"x":8.606694975806844,"y":142.58050549798597}},
-// {"score":0.00172048166859895,"part":"leftEye","position":{"x":-1.971327473704417,"y":147.7190159660863}},
-// {"score":0.0003588252002373338,"part":"rightEye","position":{"x":6.3466797827186525,"y":148.14438405502858}},
-// {"score":0.0009666045079939067,"part":"leftEar","position":{"x":0.9239634783145232,"y":135.73298527644232}},
-// {"score":0.000577143335249275,"part":"rightEar","position":{"x":4.604748801768477,"y":180.42160700363826}},
-// {"score":0.0013480210909619927,"part":"leftShoulder","position":{"x":586.5796241224649,"y":426.8325367885915}},
-// {"score":0.0030843219719827175,"part":"rightShoulder","position":{"x":3.466550972829929,"y":154.52390040280667}},
-// {"score":0.002880990970879793,"part":"leftElbow","position":{"x":-4.7106914550019505,"y":393.8237903618763}},
-// {"score":0.009597140364348888,"part":"rightElbow","position":{"x":-8.318128184111739,"y":398.9034786739865}},
-// {"score":0.00475017074495554,"part":"leftWrist","position":{"x":-12.120985010298849,"y":200.60858522284303}},
-// {"score":0.0070198411121964455,"part":"rightWrist","position":{"x":-9.45412479585121,"y":200.77998107783264}},
-// {"score":0.0031873940024524927,"part":"leftHip","position":{"x":-2.842082054864226,"y":131.5514197716346}},
-// {"score":0.006636374164372683,"part":"rightHip","position":{"x":5.314648608893575,"y":261.33988029495845}},
-// {"score":0.0021908734925091267,"part":"leftKnee","position":{"x":-6.028505227123148,"y":196.26328307724793}},
-// {"score":0.003443994326516986,"part":"rightKnee","position":{"x":-0.6007099002832184,"y":461.7512750129938}},
-// {"score":0.0013792843092232943,"part":"leftAnkle","position":{"x":0.01280265646680097,"y":466.98959889877864}},
-// {"score":0.0029822608921676874,"part":"rightAnkle","position":{"x":0.45653270298902776,"y":468.7541722160863}}]}
+// [{"score":0.0005129482597112656,"part":"nose","position":{"x":8.606694975806844,"y":142.58050549798597}}, 0
+// {"score":0.00172048166859895,"part":"leftEye","position":{"x":-1.971327473704417,"y":147.7190159660863}}, 1
+// {"score":0.0003588252002373338,"part":"rightEye","position":{"x":6.3466797827186525,"y":148.14438405502858}}, 2
+// {"score":0.0009666045079939067,"part":"leftEar","position":{"x":0.9239634783145232,"y":135.73298527644232}}, 3
+// {"score":0.000577143335249275,"part":"rightEar","position":{"x":4.604748801768477,"y":180.42160700363826}}, 4 
+// {"score":0.0013480210909619927,"part":"leftShoulder","position":{"x":586.5796241224649,"y":426.8325367885915}}, 5
+// {"score":0.0030843219719827175,"part":"rightShoulder","position":{"x":3.466550972829929,"y":154.52390040280667}}, 6
+// {"score":0.002880990970879793,"part":"leftElbow","position":{"x":-4.7106914550019505,"y":393.8237903618763}}, 7
+// {"score":0.009597140364348888,"part":"rightElbow","position":{"x":-8.318128184111739,"y":398.9034786739865}}, 8
+// {"score":0.00475017074495554,"part":"leftWrist","position":{"x":-12.120985010298849,"y":200.60858522284303}}, 9 
+// {"score":0.0070198411121964455,"part":"rightWrist","position":{"x":-9.45412479585121,"y":200.77998107783264}}, 10
+// {"score":0.0031873940024524927,"part":"leftHip","position":{"x":-2.842082054864226,"y":131.5514197716346}}, 11
+// {"score":0.006636374164372683,"part":"rightHip","position":{"x":5.314648608893575,"y":261.33988029495845}},  12
+// {"score":0.0021908734925091267,"part":"leftKnee","position":{"x":-6.028505227123148,"y":196.26328307724793}}, 13
+// {"score":0.003443994326516986,"part":"rightKnee","position":{"x":-0.6007099002832184,"y":461.7512750129938}}, 14
+// {"score":0.0013792843092232943,"part":"leftAnkle","position":{"x":0.01280265646680097,"y":466.98959889877864}}, 15
+// {"score":0.0029822608921676874,"part":"rightAnkle","position":{"x":0.45653270298902776,"y":468.7541722160863}}]} 16
